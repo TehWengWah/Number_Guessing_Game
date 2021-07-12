51e0f8f27541e6d03cf8d99a8fa69b834e967b8a
TehWengWah <48057545+TehWengWah@users.noreply.github.com>
2021-07-12 15:53:45 +0800

    Update numberguess.v

--- number_guessing_game/numberguess.v
+++ number_guessing_game/numberguess.v
@@ -9,9 +9,9 @@
 	 wire [2:0] cv;
 	 wire eq;
 	 
-	 keyin U1(.DIP(DIP), .enter(enter), .datain(datain), .rst(rst));
-	 DU U2(.datain(datain), .LL(ll),.HL(hl), .CV(cv),.clk(clk),.rst(rst),.genrand(genrand),.EQ(eq));
-	 CU U3(.clk(clk),.rst(rst),.EQ(eq),.LL(ll),.HL(hl),.datain(datain),.CV(cv),.outrange(outrange),.done(done));
+	 keyin U1 (.DIP(DIP), .enter(enter), .datain(datain), .rst(rst));
+	 DU U2 (.datain(datain), .LL(ll),.HL(hl), .CV(cv),.clk(clk),.rst(rst),.genrand(genrand),.EQ(eq));
+	 CU U3 (.clk(clk),.rst(rst),.EQ(eq),.LL(ll),.HL(hl),.datain(datain),.CV(cv),.outrange(outrange),.done(done));
 	 bin2bcd U4 (.bin(ll), .bcdones(bcdllones) ,.bcdtens(bcdlltens));
 	 bcd7seg U5 (.bcd(bcdlltens), .sevenseg(sslltens));
 	 bcd7seg U6 (.bcd(bcdllones), .sevenseg(ssllones));
